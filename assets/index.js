b6f5c418b4797655920369d0c839828fa24483c1
Bruno <brussdr7@gmail.com>
2021-09-06 18:56:29 -0500

    tema de plugins y se agrego un boton que mutea el video

--- assets/index.js
+++ assets/index.js
@@ -1,9 +1,20 @@
 import MediaPlayer from "./mediaPlayer.js";
+import AutoPlay from "./plugins/autoPlay.js";
 
 const video = document.querySelector("video");
-const player = new MediaPlayer({ el: video });
+const player = new MediaPlayer(
+    { 
+        el: video,
+        plugins: [new AutoPlay],
+    }
+    );
 
-const button = document.querySelector("button");
+const button = document.querySelector("#playPause");
+const muteUnmute = document.querySelector("#unmuteMute");
+
+muteUnmute.onclick = () => {
+    player.unmuteMute();
+}
 
 button.onclick = () => {
     player.togglePlay();
--- assets/mediaPlayer.js
+++ assets/mediaPlayer.js
@@ -1,6 +1,24 @@
 function MediaPlayer(config) {
     this.media = config.el;
+    this.plugins = config.plugins || [];
+    this._initPlugins();
 }
+
+MediaPlayer.prototype._initPlugins = function () {
+    this.plugins.forEach(element => {
+        element.run(this);
+    });
+}
+
+MediaPlayer.prototype.mute = function () {
+    this.media.muted = true;
+}
+
+MediaPlayer.prototype.unmuteMute = function () {
+    this.media.muted
+    ? this.media.muted = false
+    : this.media.muted = true;
+};
 
 MediaPlayer.prototype.pause = function () {
     this.media.pause();
