caf32b6edc1b08dfd2fac904b2c1d6ab14634416
Bruno <brussdr7@gmail.com>
2022-02-19 14:22:55 -0600

    tipado de clases en TypeScript

--- assets/mediaPlayer.js
+++ assets/mediaPlayer.js
@@ -1,37 +1,34 @@
-function MediaPlayer(config) {
-    this.media = config.el;
-    this.plugins = config.plugins || [];
-    this._initPlugins();
-}
-
-MediaPlayer.prototype._initPlugins = function () {
-    this.plugins.forEach(element => {
-        element.run(this);
-    });
-}
-
-MediaPlayer.prototype.mute = function () {
-    this.media.muted = true;
-}
-
-MediaPlayer.prototype.unmuteMute = function () {
-    this.media.muted
-    ? this.media.muted = false
-    : this.media.muted = true;
-};
-
-MediaPlayer.prototype.pause = function () {
-    this.media.pause();
-}
-MediaPlayer.prototype.play = function () {
-    this.media.play();
-}
-
-MediaPlayer.prototype.togglePlay = function () {
-    if (this.media.paused) {
-        this.media.play();
-    } else {
+class MediaPlayer {
+    constructor(config) {
+        this.media = config.el;
+        this.plugins = config.plugins || [];
+        this._initPlugins();
+    }
+    _initPlugins() {
+        this.plugins.forEach(element => {
+            element.run(this);
+        });
+    }
+    mute() {
+        this.media.muted = true;
+    }
+    unmuteMute() {
+        this.media.muted
+            ? this.media.muted = false
+            : this.media.muted = true;
+    }
+    pause() {
         this.media.pause();
+    }
+    play() {
+        this.media.play();
+    }
+    togglePlay() {
+        if (this.media.paused) {
+            this.media.play();
+        } else {
+            this.media.pause();
+        }
     }
 }
 
